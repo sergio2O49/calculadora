3f0378d1f5c440af03ec604fdfcb1a0374e1e607
SergioVeliz <velizsergio155@gmail.com>
2022-04-02 01:25:30 -0300

    calculadora terminada

---
+++ index.js
@@ -0,0 +1,14 @@
+const displayValorAnterior = document.getElementById('valor-anterio');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumero = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumero.forEach(boton =>{
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
